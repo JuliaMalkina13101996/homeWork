39b3d72c09d52ab29b5b76296a70973619622c67
Julia <119501186+JuliaMalkina13101996@users.noreply.github.com>
2022-12-13 17:09:35 +0400

    Программа по поиску максимального и минимального числа

--- MaxNumber/Program.cs
+++ MaxNumber/Program.cs
@@ -1,16 +1,100 @@
-﻿Console.WriteLine("Введите первое число: ");
-int num_A = Convert.ToInt32(Console.ReadLine());
+﻿Console.WriteLine("Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее (5,7,),(2,10,),(-9,-3)");
 
-Console.WriteLine("Введите второе число: ");
-int num_B = Convert.ToInt32(Console.ReadLine());
+int NumberA = 5;
 
-if (num_A > num_B)
+int NumberB = 7;
+
+int NumberС = 2;
+
+int NumberD = 10;
+
+int NumberE = -9;
+
+int NumberF = -3;
+
+int max = NumberA;
+
+int max2 = NumberС;
+
+int max3 = NumberE;
+
+int min = NumberA;
+
+int min2 = NumberD;
+
+int min3 = NumberF;
+
+if (NumberA < max)
 {
-    Console.WriteLine("Первое число " + num_A + " больше чем второе " + num_B);
+    max = NumberA;
 }
-else
+if (NumberB < max)
 {
-    Console.WriteLine("Второе число " + num_B + " больше чем первое " + num_A);
+    max = NumberB;
 }
 
-break;
+if (NumberС < max2)
+{
+    max2 = NumberС;
+}
+if (NumberD < max2)
+{
+    max2 = NumberD;
+}
+if (NumberE < max3)
+{
+    max3 = NumberE;
+}
+if (NumberF < max3)
+{
+    max3 = NumberF;
+}
+
+if (NumberA > min)
+{
+    min = NumberA;
+}
+
+if (NumberB > min)
+{
+    min = NumberB;
+}
+
+if (NumberС > min2)
+{
+    min2 = NumberС;
+}
+
+if (NumberD > min2)
+{
+    min2 = NumberD;
+}
+
+if (NumberE > min3)
+{
+    min3 = NumberE;
+}
+
+if (NumberF > min3)
+{
+    min3 = NumberF;
+}
+
+Console.Write("max = ");
+Console.WriteLine(max);
+
+Console.Write("max2 = ");
+Console.WriteLine(max2);
+
+Console.Write("max3 = ");
+Console.WriteLine(max3);
+
+Console.Write("min = ");
+Console.WriteLine(min);
+
+Console.Write("min2 = ");
+Console.Write(min2);
+
+
+Console.Write("min3 = ");
+Console.WriteLine(min3);
